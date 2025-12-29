c93f0b11b513e0cfa2a8b9c6a5d55c3b92b31167
OWINY-DOLLO MARVIN <owinymarvin@gmail.com>
2025-12-29 16:36:05 +0300

    added different operations for the different errors

--- restaurant/src/main.rs
+++ restaurant/src/main.rs
@@ -1,3 +1,7 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::ErrorKind; 
+
 fn main() {
     let v = vec![100, 32, 57];
     for i in &v {
@@ -9,8 +13,35 @@ fn main() {
 
     let hello = "Здравствуйте";
 
-    let s = &hello[0..1];
+    let s = &hello[0..4];
     println!("{s}");
 
-    
+    // Hash Maps
+    let text = "hello world wonderful world";
+    let mut map = HashMap::new();
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+    println!("{map:?}");
+
+    // handling errors.
+    // panic!("crash and burn");  // this causes a manual panic. Also set the panic = 'abort' in the Cargo.toml file
+
+    // Recoverable Errors.
+    let greeting_file_result = File::open("./hello.txt");
+    let greeting_file = match greeting_file_result {
+        Ok(file) => {
+            println!("File successfully opened");
+            file
+        }
+        Err(err) => match err.kind() {
+            ErrorKind::NotFound => match File::create("./hello.txt"){
+                Ok(fc) => fc, 
+                Err(err) => panic!("Problem creating the file: {err:?}"), 
+            }, 
+            other_errors => panic!("Error opening the file: {other_errors:?}")
+        }
+    };
+    println!("{greeting_file:#?}"); 
 }
